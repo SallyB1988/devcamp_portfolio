3dd21e83d934126f96cddbca1fb6f143c4d88f49
Sally Beckwith <sallypbeckwith@gmail.com>
2023-03-27 11:10:11 -0400

    Implemented edit and update functionality

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -8,6 +8,10 @@ class PortfoliosController < ApplicationController
     @portfolio_item = Portfolio.new
   end
 
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
   def create
     @portfolio_item = Portfolio.new(portfolio_params)
 
@@ -16,6 +20,18 @@ class PortfoliosController < ApplicationController
         format.html { redirect_to portfolios_url(@portfolio_item), notice: 'Portfolio item is now live.' }
       else
         format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
+
+    respond_to do |format|
+      if @portfolio_item.update(portfolio_params)
+        format.html { redirect_to portfolios_url(@blog), notice: 'Portfolio was successfully updated.' }
+      else
+        format.html { render :edit }
       end
     end
   end
