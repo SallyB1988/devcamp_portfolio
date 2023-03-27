995f6bd63c5a78332cfbe6f4152ec555800b30f9
Sally Beckwith <sallypbeckwith@gmail.com>
2023-03-27 14:57:05 -0400

    customize routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :portfolios
+  resources :portfolios, except: [:show]
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
   # controller on the left of the hash, action on the right
   get 'about-me', to: 'pages#about'
